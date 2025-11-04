d1177ceab7540c9eabe567564a5456f365594254
Henry Farquharson <henry.d.farquharson@student.uts.edu.au>
2025-11-04 21:08:24 +1100

    Update start_viewer.sh

--- start_viewer.sh
+++ start_viewer.sh
@@ -1,6 +1,6 @@
-# unclutter keeps the display clean and distraction-free
+# unclutter keeps the display clean and distraction free
 unclutter -idle 1 -root &
-#Waits 2 seconds to ensure all display services before laucnhing the image viewer
+# Waits 2 seconds to ensure all display services before laucnhing the image viewer
 sleep 2
-#Open the image viewer 'feh' in fullscreen mode with no cursor and reloads the current picture every second
+# Open the image viewer 'feh' in fullscreen mode with no cursor and reloads the current picture every second
 feh --fullscreen --auto-zoom --hide-pointer --reload 1 /home/henry/player/assets/current.jpg
