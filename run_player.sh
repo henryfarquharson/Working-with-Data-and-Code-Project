07713a1769ea2b73228dd4422af5cf24b5fc9c9e
Henry Farquharson <henry.d.farquharson@student.uts.edu.au>
2025-11-04 20:45:55 +1100

    Update run_player.sh

--- run_player.sh
+++ run_player.sh
@@ -1,11 +1,11 @@
-
+# This makes the script safer and more predictable by exiting immediately if the command fails
 set -euo pipefail
-
+# This ensures the script uses the correct Python version and dependencies
 source /home/henry/player/.venv/bin/activate
-
+# This retrieves the environment variables defined in the .env file so the script can access secrets like the API 
 set -a
 . /home/henry/player/.env
 set +a
 
-
+# This run's the main player program
 exec python /home/henry/player/player.py
